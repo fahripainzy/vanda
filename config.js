590ad5da5b3581115fe95d633936e2cda2f1f602
fahripainzy <stok3afgan@gmail.com>
2025-04-13 13:02:26 +0800

    .com.devil

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  BOT_TOKEN: "7635784932:AAG-uzF8BeS0cSOeZP4klGfo_Niy7tr3_4w,
+  OWNER_ID: ["6758060721"],
+};
+
+/*/━━━━━━━━━━━━━━━━━━  
+
+🚀 Developer  : Fahrizy    
+🛠️ Version    : 1.0  
+🛢️ Tele       : t.me/fahrizyteamznx
+
+⚠️ **警告!** ⚠️  
+📌 此机器人使用数据库。  
+📌 如果您的令牌 **未在数据库中注册**，  
+📌 机器人将 **无法运行**！  
+📌 请确保您的令牌已注册后再使用本机器人。  
+
+⚠️ **PERINGATAN!** ⚠️  
+📌 Bot ini menggunakan database.  
+📌 Jika token Anda **tidak terdaftar di database**,  
+📌 bot **tidak akan berjalan**
+📌 Pastikan token Anda sudah terdaftar sebelum menggunakan bot ini.  
+
+━━━━━━━━━━━━━━━━━━━/*/
